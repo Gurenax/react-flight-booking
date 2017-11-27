9b43835df486ef2cd66e801169188f6705a1fe8d
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-27 11:20:52 +1100

    Add button component

---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+function Button(props) {
+  let className = 'btn'
+
+  if (props.primary) {
+    className += ' btn--primary'
+  }
+
+  if (props.magic) {
+    className += ' btn--rainbow'
+  }
+  
+  return (
+    <button className={ className } >
+      { props.title }
+    </button>
+  )
+}
+
+export default Button
